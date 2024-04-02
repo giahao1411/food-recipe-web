e8ac04e6a73eed1789de582878fe50cf503ea131
anhkhoakz <113995598+anhkhoakz@users.noreply.github.com>
2024-04-02 11:07:59 +0700

    Update login form

--- js/logIn_script.js
+++ js/logIn_script.js
@@ -1,11 +1,11 @@
-const container = document.getElementById('container');
-const registerBtn = document.getElementById('register');
-const loginBtn = document.getElementById('login');
+const container = document.getElementById("container");
+const registerBtn = document.getElementById("register");
+const loginBtn = document.getElementById("login");
 
-registerBtn.addEventListener('click', () => {
+registerBtn.addEventListener("click", () => {
     container.classList.add("active");
 });
 
-loginBtn.addEventListener('click', () => {
+loginBtn.addEventListener("click", () => {
     container.classList.remove("active");
 });
