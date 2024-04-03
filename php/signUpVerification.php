64dfa4a6bfe901b176a582169e4686ec4ed45655
unknown <trangiahao498@gmail.com>
2024-04-03 16:30:53 +0700

    sign up

--- php/signUpVerification.php
+++ php/signUpVerification.php
@@ -1,1 +1,11 @@
 <?php
+
+function isDisposableEmail($email, $blocklist_path = null)
+{
+    if (!$blocklist_path) $blocklist_path = __DIR__ . '/disposable_email_blocklist.conf';
+    $disposable_domains = file($blocklist_path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+    $domain = mb_strtolower(explode('@', trim($email))[1]);
+    return in_array($domain, $disposable_domains);
+}
+
+if ()
