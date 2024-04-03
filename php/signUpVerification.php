d54b8b50d24e84f36581ff3dbede6e824366ebf8
anhkhoakz <113995598+anhkhoakz@users.noreply.github.com>
2024-04-03 16:40:44 +0700

    Update checkPassword

--- php/signUpVerification.php
+++ php/signUpVerification.php
@@ -1,11 +1,12 @@
 <?php
 
-function isDisposableEmail($email, $blocklist_path = null)
+function isDisposableEmail($email)
 {
-    if (!$blocklist_path) $blocklist_path = __DIR__ . '/disposable_email_blocklist.conf';
+    $blocklist_path = __DIR__ . '../data/spamEmails/disposable_email_blocklist.conf';
     $disposable_domains = file($blocklist_path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
     $domain = mb_strtolower(explode('@', trim($email))[1]);
     return in_array($domain, $disposable_domains);
+    // Source: https://github.com/disposable-email-domains/disposable-email-domains/
 }
 
 if ()
