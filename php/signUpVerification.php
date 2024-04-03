a01081fdd096d6d2a71fb6380b1760739649d97d
unknown <trangiahao498@gmail.com>
2024-04-03 16:41:05 +0700

    sign up update

--- php/signUpVerification.php
+++ php/signUpVerification.php
@@ -8,4 +8,53 @@ function isDisposableEmail($email, $blocklist_path = null)
     return in_array($domain, $disposable_domains);
 }
 
-if ()
+function checkCommonPassword($password)
+{
+    $commonPasswords = file("common_passwords_list.txt", FILE_IGNORE_NEW_LINES);
+
+    return in_array($password, $commonPasswords);
+}
+
+function validateSignUpEmail($email)
+{
+    $re = '/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/';
+    if (
+        $email === null ||
+        strlen(trim($email)) === 0 ||
+        !preg_match($re, $email) ||
+        isDisposableEmail($email)
+    ) {
+        return false;
+    }
+    return true;
+}
+
+function validateSignUpPassowrd($password)
+{
+    if ($password === null || strlen($password) < 8) {
+        return false;
+    }
+    $uppercaseRegex = '/[A-Z]/';
+    $lowercaseRegex = '/[a-z]/';
+    $specialCharRegex = '/[!@#$%^&*()_+\-=[\]{};\'":\\\\|,.<>\/?]/';
+    if (
+        !preg_match($uppercaseRegex, $password) ||
+        !preg_match($lowercaseRegex, $password) ||
+        !preg_match($specialCharRegex, $password) ||
+        checkCommonPassword($password)
+    ) {
+        return false;
+    }
+    return true;
+}
+
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    // retrive form data
+    $userName = $_POST["username"];
+    $userEmail = $_POST["email"];
+    $userPassword = $_POST["password"];
+
+    if (validateSignUpEmail($userEmail) && validateSignUpPassowrd($userPassword)) {
+    } else {
+    }
+}
