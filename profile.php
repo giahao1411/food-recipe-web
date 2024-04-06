98b7db8f4aac960cb13354ec7b3b3bc4f8d10152
anhkhoakz <113995598+anhkhoakz@users.noreply.github.com>
2024-04-06 11:25:26 +0700

    Add change password button and add edit function but not correctly format yet

--- profile.php
+++ profile.php
@@ -32,9 +32,12 @@ session_start();
                         <div class="card-body">
                             <img src="imgs/profile-picture.jpg" class="rounded-circle" width="150">
                             <div class="mt-3">
-                                <h4 style="padding-bottom: 50px;"><?= $_POST['username'] ?></h4>
+                                <h4 style="padding-bottom: 50px;">
+                                    <?= $_POST['username'] ?>
+                                </h4>
                                 <!-- Add Recipe button -->
-                                <div class="dashbroad-btn"><a href="#" data-bs-toggle="modal" data-bs-target="#editor">Add Recipe</a></div>
+                                <div class="dashbroad-btn"><a href="#" data-bs-toggle="modal"
+                                        data-bs-target="#editor">Add Recipe</a></div>
                                 <!-- End of Add Recipe button -->
                                 <div class="dashbroad-btn"><a href="posts/privacy-policy.php">Privacy Policy</a></div>
                                 <div class="dashbroad-btn"><a href="posts/LICENSE.php">License</a></div>
@@ -51,7 +54,10 @@ session_start();
                                     <h5>Username</h5>
                                 </div>
                                 <div class="col-md-9 text-secondary">
-                                    <?php echo $_POST['username'] ?>
+                                    <span id="currentUsername">
+                                        <?php echo $_POST['username'] ?>
+                                    </span>
+                                    <input type="text" id="usernameInput" style="display: none;">
                                 </div>
                             </div>
                             <hr>
@@ -60,15 +66,29 @@ session_start();
                                     <h5>Email</h5>
                                 </div>
                                 <div class="col-md-9 text-secondary">
-                                    <?php echo $_POST['email'] ?>
+                                    <span id="currentEmail">
+                                        <?php echo $_POST['email'] ?>
+                                    </span>
+                                    <input type="text" id="emailInput" style="display: none;">
                                 </div>
                                 <div class="col-md-3 mt-3">
-                                    <button type="button" class="px-4 btn btn-primary" data-toggle="button" aria-pressed="false" autocomplete="off">Edit</button>
+                                    <button type="button" class="px-4 btn btn-primary" onclick="editProfile()"
+                                        id="editButton">Edit</button>
                                 </div>
                             </div>
-
                         </div>
                     </div>
+
+                    <div class="card mb-3 content">
+                        <h1 class="m-3 pb-5">Change Password</h1>
+                        <div class="col-md-3 m-3">
+                            <button type="button" class="btn btn-primary" data-bs-toggle="modal"
+                                data-bs-target="#changePasswordModal">Change Password</button>
+                        </div>
+                    </div>
+
+
+
                     <div class="card mb-3 content">
                         <h1 class="m-3 pb-5">Recent Activities</h1>
                     </div>
@@ -101,8 +121,44 @@ session_start();
     </div>
     <!-- End of Quill editor modal -->
 
+    <!-- Password change form modal -->
+    <div class="modal fade" id="changePasswordModal" tabindex="-1" aria-labelledby="exampleModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Change Password</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form id="changePasswordForm">
+                        <div class="mb-3">
+                            <label for="oldPassword" class="form-label">Old Password</label>
+                            <input type="password" class="form-control" id="oldPassword" required>
+                        </div>
+                        <div class="mb-3">
+                            <label for="newPassword" class="form-label">New Password</label>
+                            <input type="password" class="form-control" id="newPassword" required>
+                        </div>
+                        <div class="mb-3">
+                            <label for="confirmPassword" class="form-label">Confirm New Password</label>
+                            <input type="password" class="form-control" id="confirmPassword" required>
+                        </div>
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary" id="savePasswordButton">Save</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End of Password change form modal -->
+
     <!--CDN-->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/quill@2.0.0-rc.5/dist/quill.js"></script>
 
     <script>
@@ -110,6 +166,8 @@ session_start();
             theme: 'snow'
         });
     </script>
+
+    <script src="js/edit_profile.js"></script>
 
 </body>
 
